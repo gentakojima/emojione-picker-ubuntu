be87a0a994084b8e0d6273f8100e9eeda4ac0a28
Jorge Suárez de Lis <yo@jorgesuarezdelis.name>
2016-06-12 20:05:29 +0200

    Add exit option and a .desktop file to open from the dash

--- install.sh
+++ install.sh
@@ -2,13 +2,16 @@
 
 _INSTALL_PREFIX=/usr/local/
 _AUTOSTART_DIR=/etc/xdg/autostart
+_APP_DIR=/usr/local/share/applications
 
 uninstall_emojionepicker(){
 	# Remove files
 	rm -f $_INSTALL_PREFIX/bin/emojione-picker
 	rm -f $_AUTOSTART_DIR/emojione-picker_autostart.desktop
 	rm -rf $_INSTALL_PREFIX/share/emojione-picker
-	# Uninstall icons
+    # Remove desktop entry
+    rm -f $_APP_DIR/emojione-picker.desktop 
+	# Uninstall iconsa
 	for SIZE in 16 22 24 32 48 64 ; do
 		xdg-icon-resource uninstall --theme ubuntu-mono-dark --size ${SIZE} emojione-picker
 		xdg-icon-resource uninstall --theme ubuntu-mono-light --size ${SIZE} emojione-picker
@@ -24,12 +27,16 @@ install_emojionepicker(){
 	cp emojione-picker $_INSTALL_PREFIX/bin
 	cp emojione-picker_autostart.desktop $_AUTOSTART_DIR
 	cp -rf assets $_INSTALL_PREFIX/share/emojione-picker/
+    # Fix autostart paths
 	sed -i $_AUTOSTART_DIR/emojione-picker_autostart.desktop -e "s*/usr*$_INSTALL_PREFIX*g"
 	# Install icons
 	for SIZE in 16 22 24 32 48 64 ; do
 		xdg-icon-resource install --theme ubuntu-mono-dark --size ${SIZE} assets/icon-mono-dark-${SIZE}.png emojione-picker
 		xdg-icon-resource install --theme ubuntu-mono-light --size ${SIZE} assets/icon-mono-light-${SIZE}.png emojione-picker
 	done
+    # Install desktop entry
+    mkdir -p $_APP_DIR
+    cp -f emojione-picker.desktop $_APP_DIR
 }
 
 cd "${0%/*}"
@@ -38,9 +45,11 @@ if [ "$(id -u)" != "0" ]; then
 	echo "Since you are running this as plain user, the program will be installed just for the current user."
 	_INSTALL_PREFIX=~/.local
 	_AUTOSTART_DIR=~/.config/autostart
+    _APP_DIR=~/.local/share/applications
 else
 	_INSTALL_PREFIX=/usr/local
 	_AUTOSTART_DIR=/etc/xdg/autostart
+    _APP_DIR=/usr/local/share/applications
 fi
 
 if [ -f "$_INSTALL_PREFIX/bin/emojione-picker" ] ; then
